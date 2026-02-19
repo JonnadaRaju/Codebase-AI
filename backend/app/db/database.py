a4a4bd74dca0a52c8be84417d82707f20cec458f
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-19 17:07:36 +0530

    modified database to postgres

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL,pool_pre_ping=True, pool_size=10,max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
