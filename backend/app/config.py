6bc1d4295554792bd5bb7a902009ee87288d8cea
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-19 18:40:26 +0530

    Changed model from GROQ to Gemini

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,8 +2,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Groq LLM
-    GROQ_API_KEY: str
-    GROQ_MODEL: str = "llama-3.1-8b-instant"
+    # GROQ_API_KEY: str
+    # GROQ_MODEL: str = "llama-3.1-8b-instant"
+    
+    # Gemini LLM
+    GEMINI_API_KEY: str
+    GEMINI_MODEL: str = "gemini-2.0-flash"
 
     # Embedding Model
     EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
@@ -15,10 +19,10 @@ class Settings(BaseSettings):
     CHROMA_PERSIST_DIR: str = "./chroma_db"
 
     # Chunking
-    CHUNK_SIZE: int = 500
+    CHUNK_SIZE: int = 400
     CHUNK_OVERLAP: int = 50
     MAX_FILE_SIZE_KB: int = 500
-    TOP_K_RETRIEVAL: int = 3
+    TOP_K_RETRIEVAL: int = 10
 
     class Config:
         env_file = ".env"
